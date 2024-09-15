ff6447100b15991b148e0dc69bf2596bdf74177f
wroncall <wroncall@cuc.edu.co>
2024-09-14 23:22:09 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import milogo from "./assets/img/realmadrid.png"
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={milogo} alt= "Logo real madrid"/>
+      <h1>¡¡¡REAL MADRID EL EQUIPO MAS GRANDE DE EUROPA!!!</h1>
+      <p>El club se fundo el 6 de marzo de 1902, 122 años de gloria</p>
+    </div>
+    </>
+  )
+}
+
+export default App
